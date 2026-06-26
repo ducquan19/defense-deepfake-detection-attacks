a643c4d4ce3bbf5d169ee852f9f9baa8b5647b2b
Tran Duc Quan <ducq317@gmail.com>
2026-06-26 21:17:49 +0700

    feat: add defense method

--- src/defenses/__init__.py
+++ src/defenses/__init__.py
@@ -1,1 +1,16 @@
 """Adversarial defense implementations."""
+
+from defenses.preprocessing import JPEGSmoothingDefense, jpeg_smoothing_batch
+from defenses.adversarial_training import AdversarialTrainer, adversarial_train_one_epoch
+
+DEFENSE_REGISTRY: dict[str, type] = {
+    "jpeg_smoothing": JPEGSmoothingDefense,
+}
+
+__all__ = [
+    "JPEGSmoothingDefense",
+    "jpeg_smoothing_batch",
+    "AdversarialTrainer",
+    "adversarial_train_one_epoch",
+    "DEFENSE_REGISTRY",
+]
